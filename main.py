166c13fbd447e26f2e5d123dd579745579f4da25
DqrkEvil <DqrkEvil11@gmail.com>
2024-04-17 09:00:15 +0200

    Fix divide by zero bug

--- main.py
+++ main.py
@@ -1,64 +1,67 @@
 import PySimpleGUI as sg
 
 def createWindow(theme):
-	sg.theme(theme)
-	sg.set_options(font = "Arial 14", button_element_size = (6,3))
-	buttonSize = (6,3)
-	layout = [
-		[sg.Text(
-			"", 
-			font = "Arial 26", 
-			justification = "right", 
-			expand_x = True, 
-			pad = (10,20),
-			right_click_menu = themeMenu,
-			key = "-TEXT-")
-		],
-		[sg.Button("Clear", expand_x = True), sg.Button("Enter", expand_x = True)],
-		[sg.Button(7, size = buttonSize),sg.Button(8, size = buttonSize),sg.Button(9, size = buttonSize),sg.Button("*", size = buttonSize)],
-		[sg.Button(4, size = buttonSize),sg.Button(5, size = buttonSize),sg.Button(6, size = buttonSize),sg.Button("/", size = buttonSize)],
-		[sg.Button(1, size = buttonSize),sg.Button(2, size = buttonSize),sg.Button(3, size = buttonSize),sg.Button("-", size = buttonSize)],
-		[sg.Button(0, expand_x = True),sg.Button(".", size = buttonSize),sg.Button("+", size = buttonSize)],
-	]
+    sg.theme(theme)
+    sg.set_options(font="Arial 14", button_element_size=(6, 3))
+    buttonSize = (6, 3)
+    layout = [
+        [sg.Text(
+            "",
+            font="Arial 26",
+            justification="right",
+            expand_x=True,
+            pad=(10, 20),
+            right_click_menu=themeMenu,
+            key="-TEXT-")
+        ],
+        [sg.Button("Clear", expand_x=True), sg.Button("Enter", expand_x=True)],
+        [sg.Button(7, size=buttonSize), sg.Button(8, size=buttonSize), sg.Button(9, size=buttonSize), sg.Button("*", size=buttonSize)],
+        [sg.Button(4, size=buttonSize), sg.Button(5, size=buttonSize), sg.Button(6, size=buttonSize), sg.Button("/", size=buttonSize)],
+        [sg.Button(1, size=buttonSize), sg.Button(2, size=buttonSize), sg.Button(3, size=buttonSize), sg.Button("-", size=buttonSize)],
+        [sg.Button(0, expand_x=True), sg.Button(".", size=buttonSize), sg.Button("+", size=buttonSize)],
+    ]
 
-	return sg.Window("Calculator", layout)
+    return sg.Window("Calculator", layout)
 
-themeMenu = ["menu",["LightGrey1","Darkblue9","DarkGray8","random"]]
+themeMenu = ["menu", ["LightGrey1", "Darkblue9", "DarkGray8", "random"]]
 window = createWindow("DarkBlue9")
 
 currentNum = []
 fullOperation = []
 
 while True:
-	event, values = window.read()
-	if event == sg.WIN_CLOSED:
-		break
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
 
-	if event in themeMenu[1]:
-		window.close()
-		window = createWindow(event)
-		
-	if event in ["0","1","2","3","4","5","6","7","8","9","."]:
-		currentNum.append(event)
-		numString = "".join(currentNum)
-		window["-TEXT-"].update(numString)
+    if event in themeMenu[1]:
+        window.close()
+        window = createWindow(event)
 
-	if event in ["+","-","/","*"]:
-		fullOperation.append("".join(currentNum))
-		currentNum = []
-		fullOperation.append(event)
-		window["-TEXT-"].update("")
+    if event in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."]:
+        currentNum.append(event)
+        numString = "".join(currentNum)
+        window["-TEXT-"].update(numString)
 
-	if event == "Enter":
-		fullOperation.append("".join(currentNum))
-		result = eval(" ".join(fullOperation))
-		window["-TEXT-"].update(result)
-		currentNum = fullOperation
-		fullOperation = []
+    if event in ["+", "-", "/", "*"]:
+        fullOperation.append("".join(currentNum))
+        currentNum = []
+        fullOperation.append(event)
+        window["-TEXT-"].update("")
 
-	if event == "Clear":
-		currentNum = []
-		fullOperation = []
-		window["-TEXT-"].update("")
+    if event == "Enter":
+        fullOperation.append("".join(currentNum))
+        try:
+            result = eval(" ".join(fullOperation))
+        except ZeroDivisionError:
+            result = "Cannot divide by zero"
+        window["-TEXT-"].update(result)
+        currentNum = fullOperation
+        fullOperation = []
+
+    if event == "Clear":
+        currentNum = []
+        fullOperation = []
+        window["-TEXT-"].update("")
 
 window.close()
