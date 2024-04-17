6e7e2517b0c9d3dc175eab4f1915f5f9a3b985dc
DqrkEvil <DqrkEvil11@gmail.com>
2024-04-17 08:57:45 +0200

    Update code style

--- main.py
+++ main.py
@@ -1,64 +1,64 @@
 import PySimpleGUI as sg
 
-def create_window(theme):
+def createWindow(theme):
 	sg.theme(theme)
-	sg.set_options(font = 'Arial 14', button_element_size = (6,3))
-	button_size = (6,3)
+	sg.set_options(font = "Arial 14", button_element_size = (6,3))
+	buttonSize = (6,3)
 	layout = [
 		[sg.Text(
-			'', 
-			font = 'Arial 26', 
-			justification = 'right', 
+			"", 
+			font = "Arial 26", 
+			justification = "right", 
 			expand_x = True, 
 			pad = (10,20),
-			right_click_menu = theme_menu,
-			key = '-TEXT-')
+			right_click_menu = themeMenu,
+			key = "-TEXT-")
 		],
-		[sg.Button('Clear', expand_x = True), sg.Button('Enter', expand_x = True)],
-		[sg.Button(7, size = button_size),sg.Button(8, size = button_size),sg.Button(9, size = button_size),sg.Button('*', size = button_size)],
-		[sg.Button(4, size = button_size),sg.Button(5, size = button_size),sg.Button(6, size = button_size),sg.Button('/', size = button_size)],
-		[sg.Button(1, size = button_size),sg.Button(2, size = button_size),sg.Button(3, size = button_size),sg.Button('-', size = button_size)],
-		[sg.Button(0, expand_x = True),sg.Button('.', size = button_size),sg.Button('+', size = button_size)],
+		[sg.Button("Clear", expand_x = True), sg.Button("Enter", expand_x = True)],
+		[sg.Button(7, size = buttonSize),sg.Button(8, size = buttonSize),sg.Button(9, size = buttonSize),sg.Button("*", size = buttonSize)],
+		[sg.Button(4, size = buttonSize),sg.Button(5, size = buttonSize),sg.Button(6, size = buttonSize),sg.Button("/", size = buttonSize)],
+		[sg.Button(1, size = buttonSize),sg.Button(2, size = buttonSize),sg.Button(3, size = buttonSize),sg.Button("-", size = buttonSize)],
+		[sg.Button(0, expand_x = True),sg.Button(".", size = buttonSize),sg.Button("+", size = buttonSize)],
 	]
 
-	return sg.Window('Calculator', layout)
+	return sg.Window("Calculator", layout)
 
-theme_menu = ['menu',['LightGrey1','Darkblue9','DarkGray8','random']]
-window = create_window('DarkBlue9')
+themeMenu = ["menu",["LightGrey1","Darkblue9","DarkGray8","random"]]
+window = createWindow("DarkBlue9")
 
-current_num = []
-full_operation = []
+currentNum = []
+fullOperation = []
 
 while True:
 	event, values = window.read()
 	if event == sg.WIN_CLOSED:
 		break
 
-	if event in theme_menu[1]:
+	if event in themeMenu[1]:
 		window.close()
-		window = create_window(event)
+		window = createWindow(event)
 		
-	if event in ['0','1','2','3','4','5','6','7','8','9','.']:
-		current_num.append(event)
-		num_string = ''.join(current_num)
-		window['-TEXT-'].update(num_string)
+	if event in ["0","1","2","3","4","5","6","7","8","9","."]:
+		currentNum.append(event)
+		numString = "".join(currentNum)
+		window["-TEXT-"].update(numString)
 
-	if event in ['+','-','/','*']:
-		full_operation.append(''.join(current_num))
-		current_num = []
-		full_operation.append(event)
-		window['-TEXT-'].update('')
+	if event in ["+","-","/","*"]:
+		fullOperation.append("".join(currentNum))
+		currentNum = []
+		fullOperation.append(event)
+		window["-TEXT-"].update("")
 
-	if event == 'Enter':
-		full_operation.append(''.join(current_num))
-		result = eval(' '.join(full_operation))
-		window['-TEXT-'].update(result)
-		current_num = full_operation
-		full_operation = []
+	if event == "Enter":
+		fullOperation.append("".join(currentNum))
+		result = eval(" ".join(fullOperation))
+		window["-TEXT-"].update(result)
+		currentNum = fullOperation
+		fullOperation = []
 
-	if event == 'Clear':
-		current_num = []
-		full_operation = []
-		window['-TEXT-'].update('')
+	if event == "Clear":
+		currentNum = []
+		fullOperation = []
+		window["-TEXT-"].update("")
 
 window.close()
